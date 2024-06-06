9de87dd3e50905310736b53d1827508f4459a5d6
jcst2019 <juansolar_4@hotmail.com>
2024-06-06 18:40:49 -0500

    Crear EncryptionAESUtil

--- src/main/java/com/org/reto/indra/constants/ConstantsUtil.java
+++ src/main/java/com/org/reto/indra/constants/ConstantsUtil.java
@@ -1,6 +1,13 @@
 package com.org.reto.indra.constants;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class ConstantsUtil {
+
     public static final String AES = "AES";
     public static final int AES_KEY_SIZE = 256;
+    public static final String AES_KEY_BASE64 = "reto_indra_key_aes";
+
 }
--- src/main/java/com/org/reto/indra/util/EncryptionAESUtil.java
+++ src/main/java/com/org/reto/indra/util/EncryptionAESUtil.java
@@ -7,18 +7,23 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class EncryptionAESUtil {
 
-    // Generar una clave AES
     public static SecretKey generateAESKey() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance(ConstantsUtil.AES);
         keyGen.init(ConstantsUtil.AES_KEY_SIZE);
         return keyGen.generateKey();
     }
 
-    // Encriptar un texto usando AES
+    public static SecretKey getAESKey() {
+        byte[] decodedKey = Base64.getDecoder().decode(ConstantsUtil.AES_KEY_BASE64);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, ConstantsUtil.AES);
+    }
+
+
     public static String encryptAES(String plainText, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance(ConstantsUtil.AES);
         cipher.init(Cipher.ENCRYPT_MODE, key);
@@ -26,22 +31,10 @@ public class EncryptionAESUtil {
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    // Desencriptar un texto usando AES
     public static String decryptAES(String encryptedText, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance(ConstantsUtil.AES);
         cipher.init(Cipher.DECRYPT_MODE, key);
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
         return new String(decryptedBytes);
-    }
-
-    // Convertir una clave AES a una cadena Base64
-    public static String keyToString(SecretKey key) {
-        return Base64.getEncoder().encodeToString(key.getEncoded());
-    }
-
-    // Convertir una cadena Base64 a una clave AES
-    public static SecretKey stringToKey(String keyString) {
-        byte[] decodedKey = Base64.getDecoder().decode(keyString);
-        return new SecretKeySpec(decodedKey, 0, decodedKey.length, ConstantsUtil.AES);
     }
 }
